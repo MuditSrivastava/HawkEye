96a582a43e6c2dd6486b33b304da720c5a9c7855
Mudit <srivastava.mudit2908@gmail.com>
2017-03-17 18:48:19 +0530

    Handling Permission

--- HawkEye/app/src/main/java/com/mudit/android/hawkeye/MainActivity.java
+++ HawkEye/app/src/main/java/com/mudit/android/hawkeye/MainActivity.java
@@ -1,11 +1,21 @@
 package com.mudit.android.hawkeye;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -15,14 +25,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.squareup.picasso.Picasso;
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import com.ibm.watson.developer_cloud.android.library.audio.StreamPlayer;
 import com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech;
 import com.ibm.watson.developer_cloud.text_to_speech.v1.model.Voice;
 import com.wang.avi.*;
+
+import static android.R.attr.data;
+import static android.R.attr.path;
+import static com.ibm.watson.developer_cloud.android.library.camera.CameraHelper.REQUEST_IMAGE_CAPTURE;
 
 public class MainActivity extends AppCompatActivity implements AsyncResponse{
 
@@ -36,11 +55,22 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse{
     private AVLoadingIndicatorView avi;
     private TextView loadtext;
     private StreamPlayer streamPlayer;
-
+    private  int permissionCheck1, permissionCheck2,permissionCheck3;
+    private static final int MY_PERMISSIONS_REQUEST_READ_STORAGE=245;
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_STORAGE=121;
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA=174;
+    private Uri photoURI;
+    private File photoFile;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        permissionCheck1 = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        permissionCheck2 = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+        permissionCheck3 = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA  );
         this.imageView = (ImageView)this.findViewById(R.id.imageView1);
         recyclerView=(RecyclerView)this.findViewById(R.id.recycler_View);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -49,19 +79,36 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse{
         loadtext=(TextView)this.findViewById(R.id.loadtext);
         avi =(AVLoadingIndicatorView)this.findViewById(R.id.indicator);
         stopAnim();
+
+        checkPermisson();
+
+
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-            Uri tempUri = data.getData();
-            File finalFile = new File(getPath(tempUri));
-            Picasso.with(this)
-                    .load(finalFile)
-                    .placeholder(R.drawable.ph1)
-                    .into(imageView);
-            startAnim();
-            FetchTask search = new FetchTask(finalFile,this);
-            search.execute();
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK ) {
+            if(android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                Uri tempUri = data.getData();
+                File finalFile = new File(getPath(tempUri));
+                Picasso.with(this)
+                        .load(finalFile)
+                        .placeholder(R.drawable.ph1)
+                        .into(imageView);
+                startAnim();
+                FetchTask search = new FetchTask(finalFile,this);
+                search.execute();
+
+            }
+            else {
+                        Picasso.with(this)
+                        .load(photoFile)
+                        .placeholder(R.drawable.ph1)
+                        .into(imageView);
+                startAnim();
+                FetchTask search = new FetchTask(photoFile, this);
+                search.execute();
+            }
+
         }
         if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
             Uri selectedImage = data.getData();
@@ -139,19 +186,61 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse{
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if (item.getItemId() == R.id.action_camera) {
-            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        if(this.isInternetConnectionPresent()){
 
+
+        if (item.getItemId() == R.id.action_camera) {
+            if(permissionCheck1==PackageManager.PERMISSION_GRANTED && permissionCheck2==PackageManager.PERMISSION_GRANTED && permissionCheck3==PackageManager.PERMISSION_GRANTED) {
+
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                if(android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                    startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+                }else{
+
+                if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+                    // Create the File where the photo should go
+                    photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException ex) {
+                        // Error occurred while creating the File
+                    }
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                         photoURI = FileProvider.getUriForFile(this,
+                                "com.mudit.android.hawkeye.fileprovider",
+                                photoFile);
+                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+                    }
+                }
+
+
+            }}
+            else{
+                checkPermisson();
+
+            }
 
         }
         if(item.getItemId()==R.id.action_browse){
+            if(permissionCheck1==PackageManager.PERMISSION_GRANTED && permissionCheck2==PackageManager.PERMISSION_GRANTED) {
 
-            Intent i = new Intent(
-                    Intent.ACTION_PICK,
-                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
-            startActivityForResult(i, RESULT_LOAD_IMAGE);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+            else{
+                checkPermisson();
+
+            }
+
+        }}
+        else{
+            Toast Connect_Internet= Toast.makeText(this,"Internet Connection Required",Toast.LENGTH_LONG);
+            Connect_Internet.show();
         }
 
         return super.onOptionsItemSelected(item);
@@ -214,4 +303,45 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse{
             return null;
         }
     }
+    public boolean isInternetConnectionPresent() {
+        ConnectivityManager connectivityManager = ((ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE));
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnectedOrConnecting());
+    }
+
+  public void checkPermisson(){
+      if(permissionCheck1!=PackageManager.PERMISSION_GRANTED ) {
+          ActivityCompat.requestPermissions(this,
+                  new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},MY_PERMISSIONS_REQUEST_READ_STORAGE);
+      }
+      if( permissionCheck2!=PackageManager.PERMISSION_GRANTED) {
+          ActivityCompat.requestPermissions(this,
+                  new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},MY_PERMISSIONS_REQUEST_WRITE_STORAGE);
+      }
+      if( permissionCheck3!=PackageManager.PERMISSION_GRANTED) {
+          ActivityCompat.requestPermissions(this,
+                  new String[]{Manifest.permission.CAMERA},MY_PERMISSIONS_REQUEST_CAMERA);
+      }
+
+  }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
 }
